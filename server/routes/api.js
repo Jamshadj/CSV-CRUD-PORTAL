3a334e2f13842a95faa1a85745afcb35d36f5cdc
Jamshadj <jamshadalij2004@gmail.com>
2023-10-27 19:31:23 +0530

    CRUD operations completed

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import csvParser from 'csv-parser';
 import fs from 'fs';
+import fastcsv from "fast-csv";
+import { Readable } from 'stream';
+import { createObjectCsvWriter } from 'csv-writer';
+
 
 const router = express.Router();
 
@@ -8,77 +12,112 @@ const dataPath = '../server/data/data.csv';
 
 // Read data from CSV file
 router.get('/data', (req, res) => {
-console.log("hello");
-  const results = [];
-  fs.createReadStream(dataPath)
-    .pipe(csvParser())
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
-      res.json(results);
-    });
+  try {
+    const results = [];
+    fs.createReadStream(dataPath)
+      .pipe(csvParser())
+      .on('data', (data) => results.push(data))
+      .on('end', () => {
+        res.json(results);
+      });
+  } catch (error) {
+    res.status(500).json({ error: 'Error reading CSV file' });
+  }
 });
 
 // Create a new record in the CSV file
 router.post('/data', (req, res) => {
-
-  const newData = req.body;
- console.log("hellfo");
-  const csvData = `${newData.column1},${newData.column2},${newData.column3},${newData.column4},${newData.column5}\n`;
-
-  fs.appendFile(dataPath, csvData, (err) => {
-    if (err) {
-      res.status(500).json({ error: 'Error writing to CSV file' });
-    } else {
-      res.json({ message: 'Record added successfully' });
-    }
-  });
+  try {
+    const newData = req.body;
+    const csvData = `${newData.column1},${newData.column2},${newData.column3},${newData.column4},${newData.column5},${newData.id}\n`;
+  
+    fs.appendFileSync(dataPath, csvData);
+    res.json({ message: 'Record added successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error writing to CSV file' });
+  }
 });
 
-// Update an existing record in the CSV file
-router.put('/data/:id', (req, res) => {
+
+router.patch('/data/:id', (req, res) => {
+  try {
     const recordId = req.params.id;
     const updatedData = req.body;
-    
+    console.log(updatedData);
     const newData = [];
+    let headerSkipped = false;
+
     fs.createReadStream(dataPath)
       .pipe(csvParser())
       .on('data', (data) => {
-        if (data.id === recordId) {
-          newData.push(updatedData);
+         if (data.id === recordId) {
+          console.log("ede");
+          // Update the record with new data
+          const updatedRecord = { ...data, ...updatedData };
+          newData.push(updatedRecord);
         } else {
+          // Keep other records unchanged
           newData.push(data);
         }
       })
       .on('end', () => {
-        fs.writeFileSync(dataPath, ''); // Clear the existing file
-        newData.forEach((data) => {
-          const csvData = `${data.column1},${data.column2},${data.column3},${data.column4},${data.column5}\n`;
-          fs.appendFileSync(dataPath, csvData);
-        });
-        res.json({ message: 'Record updated successfully' });
+        // Write updated records back to the CSV file, including the header row
+        const ws = fs.createWriteStream(dataPath);
+        fastcsv
+          .write(newData, { headers: true })
+          .pipe(ws)
+          .on('finish', () => {
+            res.json({ message: 'Record updated successfully' });
+          });
       });
-  });
- 
-  // Delete a record from the CSV file
-  router.delete('/data/:id', (req, res) => {
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: 'Error updating record' });
+  }
+});
+
+// Delete a record from the CSV file
+router.delete('/data/:id', (req, res) => {
+  try {
     const recordId = req.params.id;
-  
-    const newData = [];
-    fs.createReadStream(dataPath)
-      .pipe(csvParser())
-      .on('data', (data) => {
-        if (data.id !== recordId) {
-          newData.push(data);
-        }
-      })
-      .on('end', () => {
-        fs.writeFileSync(dataPath, ''); // Clear the existing file
-        newData.forEach((data) => {
-          const csvData = `${data.column1},${data.column2},${data.column3},${data.column4},${data.column5}\n`;
-          fs.appendFileSync(dataPath, csvData);
+    console.log(recordId);
+
+    const records = [];
+    const readable = Readable.from(fs.createReadStream(dataPath).pipe(csvParser()));
+    readable.on('data', (data) => {
+      if (data.id !== recordId) {
+        records.push(data);
+      }
+    });
+
+    readable.on('end', () => {
+      const csvWriter = createObjectCsvWriter({
+  path: dataPath,
+  header: [
+    { id: 'column1', title: 'column1' },
+    { id: 'column2', title: 'column2' },
+    { id: 'column3', title: 'column3' },
+    { id: 'column4', title: 'column4' },
+    { id: 'column5', title: 'column5' },
+    { id: 'id', title: 'ID' }
+  ],
+  append: false // Overwrite the existing file
+});
+
+
+csvWriter.writeRecords(records)
+        .then(() => {
+          res.json({ message: 'Record deleted successfully' });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({ error: 'Error deleting record' });
         });
-        res.json({ message: 'Record deleted successfully' });
-      });
-  });
+    });
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ error: 'Error deleting record' });
+  }
+});
 
 export default router;
